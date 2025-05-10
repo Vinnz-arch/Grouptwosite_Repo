4e53695f2298bc05f3a812808c81e851dd8e373c
Earl Vincent <earlvincentpanrev@gmail.com>
2025-05-10 09:40:17 +0800

    3rd commit

--- crud/urls.py
+++ crud/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('gender/list', views.gender_list),
     path('gender/add', views.add_gender)
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Genders
 
 # Create your views here.
+
+def gender_list(request):
+    try:
+        genders = Genders.objects.all()
+
+        data = {
+            'genders':genders
+        }
+
+        return render(request, 'gender/GendersList.html', data)
+    except Exception as e:
+        return HttpResponse(f'Error occured during load genders: {e}')
 
 def add_gender(request):
     try:
@@ -12,7 +24,7 @@ def add_gender(request):
 
             Genders.objects.create(gender=gender).save()
             messages.success(request, 'Gender added successfully!')
-            return render(request, 'gender/AddGender.html')
+            return redirect('/gender/list')
         else:
             return render(request, 'gender/AddGender.html')     
     except Exception as e:
